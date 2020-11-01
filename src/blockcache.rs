35cb7ace5d0788976c2b424f3e54823c8c8abe78
Markus Brueckner <namenlos@geekbetrieb.de>
2020-11-01 18:40:30 +0100

    - store mapping to backup block ID in the block ID cache

--- src/blockcache.rs
+++ src/blockcache.rs
@@ -1,11 +1,14 @@
 use crate::errors::backrub_error;
 use crate::errors::Result;
+use crate::repository::BackupBlockId;
+use rmp_serde::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
 use sha3::{Digest, Sha3_256};
 
 pub trait BlockCache {
-    fn add_block(&self, data: &Vec<u8>) -> Result<()>;
+    fn add_block(&self, data: &Vec<u8>, backup_block_id: &BackupBlockId) -> Result<()>;
 
-    fn has_block(&self, data: &Vec<u8>) -> Result<bool>;
+    fn get_backup_block_id(&self, data: &Vec<u8>) -> Result<Option<BackupBlockId>>;
 }
 
 struct BlockCacheImpl<'a> {
@@ -13,24 +16,40 @@ struct BlockCacheImpl<'a> {
 }
 
 impl BlockCache for BlockCacheImpl<'_> {
-    fn add_block(&self, data: &Vec<u8>) -> Result<()> {
+    fn add_block(&self, data: &Vec<u8>, backup_block_id: &BackupBlockId) -> Result<()> {
         let mut hasher = Sha3_256::new();
         hasher.update(&data);
         let id_bytes = hasher.finalize();
         let id_str = hex::encode(id_bytes);
         let path: std::path::PathBuf = [self.path, &id_str].iter().collect();
-        std::fs::File::create(path)
+        let id_file = std::fs::File::create(path)
             .or_else(|e| backrub_error("Could not write block to block index", Some(e.into())))?;
+        backup_block_id
+            .serialize(&mut Serializer::new(id_file))
+            .or_else(|e| {
+                backrub_error(
+                    "Could not store backup block ID in block index",
+                    Some(e.into()),
+                )
+            })?;
         Ok(())
     }
 
-    fn has_block(&self, data: &Vec<u8>) -> Result<bool> {
+    fn get_backup_block_id(&self, data: &Vec<u8>) -> Result<Option<BackupBlockId>> {
         let mut hasher = Sha3_256::new();
         hasher.update(&data);
         let id_bytes = hasher.finalize();
         let id_str = hex::encode(id_bytes);
         let path: std::path::PathBuf = [self.path, &id_str].iter().collect();
-        Ok(path.exists())
+        if let Ok(block_file) = std::fs::File::open(path) {
+            let bid =
+                Deserialize::deserialize(&mut Deserializer::new(block_file)).or_else(|e| {
+                    backrub_error("Could not deserialize backup block ID", Some(e.into()))
+                })?;
+            Ok(Some(bid))
+        } else {
+            Ok(None)
+        }
     }
 }
 
--- tests/blockcachetests.rs
+++ tests/blockcachetests.rs
@@ -1,14 +1,21 @@
 #[cfg(test)]
 mod fsrepotest {
     use backrub::blockcache::BlockCache;
+    use backrub::repository::BackupBlockId;
 
     #[test]
     fn blocks_are_recognized_in_the_cache() {
         let temp = assert_fs::TempDir::new().unwrap();
         let test_path = temp.path().to_str().unwrap();
         let cache = backrub::blockcache::open(test_path).unwrap();
-        cache.add_block(&b"abcdefg".to_vec()).unwrap();
-        assert2::let_assert!(Ok(true) = cache.has_block(&b"abcdefg".to_vec()));
-        assert2::let_assert!(Ok(false) = cache.has_block(&b"zyxwvu".to_vec()));
+        let bid = BackupBlockId::from_bytes(b"01234567012345670123456701234567").unwrap();
+        cache.add_block(&b"abcdefg".to_vec(), &bid).unwrap();
+        assert2::let_assert!(
+            Ok(Some(existing_bid)) = cache.get_backup_block_id(&b"abcdefg".to_vec())
+        );
+        assert2::assert!(existing_bid == bid);
+        assert2::let_assert!(
+            Ok(Option::<BackupBlockId>::None) = cache.get_backup_block_id(&b"zyxwvu".to_vec())
+        );
     }
 }
